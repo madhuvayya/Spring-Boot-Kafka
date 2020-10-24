23266ca432f3ec99a2915f6ae4313cbd27cb642f
madhuvayya <madhu.vayya@gmail.com>
2020-10-24 23:34:19 +0530

    Initialized project setup

---
+++ src/test/java/com/bridgelabz/springbootkafkaproducer/SpringBootKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springbootkafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
